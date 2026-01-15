0f8fa37c542bd1b13c5343f50c5969bbef36481e
juedash <juedashherifi@hotmail.com>
2026-01-15 20:25:50 +0100

    add base for github page

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+
+  base: "/mortage-calculator/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
