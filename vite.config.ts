658166201811041ec0ec1836d183b4c358ea662d
juedash <juedashherifi@hotmail.com>
2026-01-16 14:52:27 +0100

    change base path

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     },
   },
 
-  base: "/mortage-calculator/",
+  base: "./",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
